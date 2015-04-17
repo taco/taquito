cb3d35cb7941fb1ac21af6b8e931be6fbbf79811
Taco <travis_johnson@volusion.com>
2015-04-17 11:20:56 -0500

    Renamed Util.js to Git.js

--- taquito.js
+++ taquito.js
@@ -4,12 +4,13 @@ var cmdargs = require('yargs').argv,
     variables = require('./variables'),
     path = require('path');
 
-var relativePath = cmdargs.path || variables.path,
+var operation = cmdargs._[0] || 'help',
+    relativePath = cmdargs.path || variables.path,
     source = cmdargs.source || variables.source,
     target = cmdargs.target || variables.target,
     remote = cmdargs.remote || variables.remote,
     skipFetch = cmdargs.skipfetch || false,
-    utils = require('./utils')({
+    git = require('./git')({
         source: source,
         target: target,
         remote: remote,
@@ -17,30 +18,71 @@ var relativePath = cmdargs.path || variables.path,
         skipFetch: skipFetch
     });
 
-utils.printLogo();
+//git.printLogo();
 
-console.log('Is ' + source + ' merged into ' + target + '?');
+//console.log('Is ' + source + ' merged into ' + target + '?');
+//
+//
 
-fs.readdirAsync(relativePath)
-    .each(function(dir) {
+var operations = {
+    fetch: function() {
         
-        if (dir.indexOf('Mozu.') !== 0 || !fs.statSync(path.join(relativePath, dir)).isDirectory()) return;
+        console.log('Fetching all branches \n');
 
-        utils.fetch(dir)
-            .then(utils.branchesExist)
-            .then(utils.merged)
-            .then(function(exists) {
-                var str = dir;
+        fs.readdirAsync(relativePath)
+            .each(function(dir) {
+                
+                if (dir.indexOf('Mozu.') !== 0 || !fs.statSync(path.join(relativePath, dir)).isDirectory()) return;
 
-                for (var i = 0; i < 30 - dir.length; i++) {
-                    str += ' ';
-                }
-
-                str += '[' + (exists ? 'X' : ' ') + ']';
-
-                console.log(str);
-            })
-            .catch(function(e) {
-                console.log(e);
+                git.fetch(dir)
+                    .catch(function(e) {
+                        console.log(e);
+                    });
             });
-    });
+    },
+
+    checkMerge: function() {
+        
+        console.log('Checking Merge: Is', source, 'merged into', target, '\n');
+
+        fs.readdirAsync(relativePath)
+            .each(function(dir) {
+                
+                if (dir.indexOf('Mozu.') !== 0 || !fs.statSync(path.join(relativePath, dir)).isDirectory()) return;
+
+                git.branchesExist(dir)
+                    .then(git.merged)
+                    .then(function(exists) {
+                        var str = dir;
+
+                        for (var i = 0; i < 30 - dir.length; i++) {
+                            str += ' ';
+                        }
+
+                        str += '[' + (exists ? 'X' : ' ') + ']';
+
+                        console.log(str);
+                    })
+                    .catch(function(e) {
+                        console.log(e);
+                    });
+            });
+    }
+}
+
+var helpers = {
+    printLogo: function() {
+        console.log('\n  ______                  _ __      \n /_  __/___ _____ ___  __(_) /_____ \n  / / / __ `/ __ `/ / / / / __/ __ \\\n / / / /_/ / /_/ / /_/ / / /_/ /_/ /\n/_/  \\__,_/\\__, /\\__,_/_/\\__/\\____/ \n             /_/                    \n');
+    }
+}
+
+helpers.printLogo();
+
+if (operations[operation]) {
+    operations[operation]();
+} else {
+    console.log('unknown command', operation);
+}
+
+
+
