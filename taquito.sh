683fb9ef269fb42f350d977e5bb10d5398ae8fa3
Taco <travis_johnson@volusion.com>
2015-05-29 18:10:22 -0500

    Added comments to taquito.sh so maybe Chester can make ps equivalent

--- taquito.sh
+++ taquito.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# Get the current directory and see if it's a Mozu Repp
 REPO=$(basename "$PWD")
+
+# Find script source so we can run node from that place
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
+
+# Fire it up
 node --harmony taquito.js $@ --dir "$REPO"
